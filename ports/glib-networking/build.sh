44189938d73b27092af576433266ee8ba33861dd
Bananymous <oskari.alaranta@bananymous.com>
2026-08-24 20:59:18 +0300

    ports: Add glib-networking port

---
+++ ports/glib-networking/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash ../install.sh
+
+NAME='glib-networking'
+VERSION='2.80.1'
+DOWNLOAD_URL="https://download.gnome.org/sources/glib-networking/${VERSION%.*}/glib-networking-$VERSION.tar.xz#b80e2874157cd55071f1b6710fa0b911d5ac5de106a9ee2a4c9c7bee61782f8e"
+DEPENDENCIES=('glib' 'openssl')
+CONFIGURE_OPTIONS=(
+	'-Dprefix=/usr'
+	'-Dbuildtype=release'
+	'-Dlibproxy=disabled'
+	'-Dgnome_proxy=disabled'
+	'-Dgnutls=disabled'
+	'-Dopenssl=enabled'
+)
+
+configure() {
+	meson setup \
+		--reconfigure \
+		--cross-file "$MESON_CROSS_FILE" \
+		"${CONFIGURE_OPTIONS[@]}" \
+		build || exit 1
+}
+
+build() {
+	meson compile -C build || exit 1
+}
+
+install() {
+	meson install --destdir="$DESTDIR" -C build || exit 1
+}
