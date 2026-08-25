1d618ea39de770a7ca34a87241a7b1309647576b
Bananymous <oskari.alaranta@bananymous.com>
2026-08-25 06:00:13 +0300

    ports: Remove circular harfbuzz/freetype/cairo dependency
    I don't even know why I was rebuilding cairo and freetype can be just
    configured with dynamic harfbuzz

--- ports/freetype/build.sh
+++ ports/freetype/build.sh
@@ -5,6 +5,24 @@ VERSION='2.14.3'
 DOWNLOAD_URL="https://download.savannah.gnu.org/releases/freetype/freetype-$VERSION.tar.xz#36bc4f1cc413335368ee656c42afca65c5a3987e8768cc28cf11ba775e785a5f"
 DEPENDENCIES=('zlib' 'libpng')
 CONFIGURE_OPTIONS=(
-	'--disable-static'
-	'lt_cv_deplibs_check_method=pass_all'
+	'-Dprefix=/usr'
+	'-Dbuildtype=release'
+	'-Dharfbuzz=dynamic'
+	'-Dc_link_args=-lgcc_s' # something is messed up with my toolchain
 )
+
+configure() {
+	meson setup \
+		--reconfigure \
+		--cross-file "$MESON_CROSS_FILE" \
+		"${CONFIGURE_OPTIONS[@]}" \
+		build || exit 1
+}
+
+build() {
+	meson compile -C build || exit 1
+}
+
+install() {
+	meson install --destdir="$DESTDIR" -C build || exit 1
+}
--- ports/harfbuzz/build.sh
+++ ports/harfbuzz/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash ../install.sh
 
 NAME='harfbuzz'
-VERSION='12.2.0'
-DOWNLOAD_URL="https://github.com/harfbuzz/harfbuzz/releases/download/$VERSION/harfbuzz-$VERSION.tar.xz#ecb603aa426a8b24665718667bda64a84c1504db7454ee4cadbd362eea64e545"
+VERSION='14.3.1'
+DOWNLOAD_URL="https://github.com/harfbuzz/harfbuzz/releases/download/$VERSION/harfbuzz-$VERSION.tar.xz#9dae9538aae2ffdf70cec31f2c27bf68e2aaeeae3112688467697d5faf6194f7"
 DEPENDENCIES=('glib' 'freetype' 'icu')
 CONFIGURE_OPTIONS=(
 	'-Dprefix=/usr'
@@ -25,24 +25,4 @@ build() {
 
 install() {
 	meson install --destdir="$DESTDIR" -C build || exit 1
-}
-
-post_install() {
-	if [ -n "$HARFBUZZ_CIRCULAR" ]; then
-		return
-	fi
-
-	export HARFBUZZ_CIRCULAR=1
-
-	pushd "$BANAN_PORT_DIR/freetype" >/dev/null || exit 1
-	rm -f .compile_hash
-	./build.sh || exit 1
-	popd >/dev/null
-
-	if grep -q cairo "$BANAN_PORT_DIR/.installed_ports"; then
-		pushd "$BANAN_PORT_DIR/cairo" >/dev/null || exit 1
-		rm -f .compile_hash
-		./build.sh || exit 1
-		popd >/dev/null
-	fi
 }
